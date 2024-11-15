b6cf1efab3b8b9b8f0ff928b96dbeb559940d3fb
Shongsu <chinhouu1992@gmail.com>
2024-11-15 17:20:46 -0500

    fix

--- android/src/main/java/com/shongsu/intent/IntentModule.java
+++ android/src/main/java/com/shongsu/intent/IntentModule.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.ReactActivity;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
