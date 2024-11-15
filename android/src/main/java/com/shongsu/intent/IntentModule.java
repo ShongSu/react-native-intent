c237796ed3721e294cd3b5d9539ba44e8b64b195
Shongsu <chinhouu1992@gmail.com>
2024-11-15 17:06:01 -0500

    Refactor

--- android/src/main/java/com/shongsu/intent/IntentModule.java
+++ android/src/main/java/com/shongsu/intent/IntentModule.java
@@ -1,66 +1,55 @@
 package com.shongsu.intent;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
-import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+import android.os.PowerManager;
+import android.os.Build;
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.facebook.react.uimanager.IllegalViewOperationException;
 
 public class IntentModule extends ReactContextBaseJavaModule {
 
-    private final ReactApplicationContext reactContext;
+  public IntentModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
 
-    public IntentModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-    }
+  @Override
+  public String getName() {
+    return "IntentModule";
+  }
 
-    @NonNull
-    @Override
-    public String getName() {
-        return "IntentModule";
-    }
+  @ReactMethod
+  public void alert(String message) {
+    Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
+  }
 
     @ReactMethod
-    public void getExtraString(String key, Promise promise) {
-        try {
-            Intent intent = getCurrentActivity().getIntent();
-            String value = intent.getStringExtra(key);
-            if (value != null) {
-                promise.resolve(value);
+    public void getBooleanExtra(String key, Promise promise) {
+        ReactActivity activity = (ReactActivity) getCurrentActivity();
+        if (activity != null) {
+            Intent intent = activity.getIntent();
+            if (intent != null && intent.hasExtra(key)) {
+                promise.resolve(intent.getBooleanExtra(key, false)); // Change type as needed
             } else {
-                promise.resolve(null); // Return null if the value is not found
+                promise.resolve(null);
             }
-        } catch (Exception e) {
-            promise.reject("ERROR", e);
-        }
-    }
-
-    @ReactMethod
-    public void getExtraBoolean(String key, Promise promise) {
-        try {
-            Intent intent = getCurrentActivity().getIntent();
-            boolean value = intent.getBooleanExtra(key, false);
-            promise.resolve(value);
-        } catch (Exception e) {
-            promise.reject("ERROR", e);
-        }
-    }
-
-    @ReactMethod
-    public void getExtraInt(String key, Promise promise) {
-        try {
-            Intent intent = getCurrentActivity().getIntent();
-            int value = intent.getIntExtra(key, -1); // Default to -1 if not found
-            promise.resolve(value);
-        } catch (Exception e) {
-            promise.reject("ERROR", e);
+        } else {
+            promise.reject("Activity doesn't exist");
         }
     }
 }
--- android/src/main/java/com/shongsu/intent/IntentPackage.java
+++ android/src/main/java/com/shongsu/intent/IntentPackage.java
@@ -1,7 +1,9 @@
 package com.shongsu.intent;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
@@ -10,15 +12,23 @@ import java.util.List;
 
 public class IntentPackage implements ReactPackage {
 
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new IntentModule(reactContext));
-        return modules;
-    }
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
-    }
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(
+                              ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new IntentModule(reactContext));
+
+    return modules;
+  }
+
 }
